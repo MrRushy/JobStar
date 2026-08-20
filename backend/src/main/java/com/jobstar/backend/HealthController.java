4f4b514583850822e2d7df28147e4a07e4fccab1
MrRushy <rushilshanmugam@gmail.com>
2026-08-19 20:52:51 -0400

    Set up React frontend and Spring Boot backend

---
+++ backend/src/main/java/com/jobstar/backend/HealthController.java
@@ -0,0 +1,16 @@
+package com.jobstar.backend;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin(origins = "http://localhost:5173")
+@RestController
+public class HealthController {
+    
+
+    @GetMapping("/api/health")
+    public String healthCheck(){
+        return "JobStar backend is running!";
+    }
+}
